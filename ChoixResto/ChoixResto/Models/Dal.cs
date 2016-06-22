d68798df6efdfe673e406b1e8867b3377518107e
Laurent-Chiaramonte <chiaramonte.laurent@gmail.com>
2016-06-22 15:34:34 +0200

    methode AjouterUtilisateur

--- ChoixResto/ChoixResto/Models/Dal.cs
+++ ChoixResto/ChoixResto/Models/Dal.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 using System.Web;
 
 namespace ChoixResto.Models
@@ -19,15 +21,46 @@ namespace ChoixResto.Models
             return bdd.Restos.ToList();
         }
 
+        public void CreerRestaurant(string nom, string telephone)
+        {
+            bdd.Restos.Add(new Resto { Nom = nom, Telephone = telephone });
+            bdd.SaveChanges();
+        }
+
+        public void ModifierRestaurant(int id, string nom, string telephone)
+        {
+            Resto restoTrouve = bdd.Restos.FirstOrDefault(resto => resto.Id == id);
+            if (restoTrouve != null)
+            {
+                restoTrouve.Nom = nom;
+                restoTrouve.Telephone = telephone;
+                bdd.SaveChanges();
+            }
+        }
+
+        public bool RestaurantExiste(string nom)
+        {
+            return bdd.Restos.Any(resto => string.Compare(resto.Nom, nom, StringComparison.CurrentCultureIgnoreCase) == 0);
+        }
+
+        public int AjouterUtilisateur(string nom, string motDePasse)
+        {
+            string motDePasseEncode = EncodeMD5(motDePasse);
+            Utilisateur utilisateur = new Utilisateur { Prenom = nom, MotDePasse = motDePasseEncode };
+            bdd.Utilisateurs.Add(utilisateur);
+            bdd.SaveChanges();
+            return utilisateur.Id;
+        }
+
         public void Dispose()
         {
             bdd.Dispose();
         }
 
-        public void CreerRestaurant(string nom, string telephone)
+        private string EncodeMD5(string motDePasse)
         {
-            bdd.Restos.Add(new Resto { Nom = nom, Telephone = telephone });
-            bdd.SaveChanges();
+            string motDePasseSel = "ChoixResto" + motDePasse + "ASP.NET MVC";
+            return BitConverter.ToString(new MD5CryptoServiceProvider().ComputeHash(ASCIIEncoding.Default.GetBytes(motDePasseSel)));
         }
 
     }
--- ChoixResto/ChoixResto/Models/IDal.cs
+++ ChoixResto/ChoixResto/Models/IDal.cs
@@ -6,9 +6,18 @@ using System.Threading.Tasks;
 
 namespace ChoixResto.Models
 {
-    interface IDal : IDisposable
+    public interface IDal : IDisposable
     {
         void CreerRestaurant(string nom, string telephone);
+        void ModifierRestaurant(int id, string nom, string telephone);
         List<Resto> ObtientTousLesRestaurants();
+        int AjouterUtilisateur(string nom, string motDePasse);
+        Utilisateur Authentifier(string nom, string motDePasse);
+        Utilisateur ObtenirUtilisateur(int id);
+        Utilisateur ObtenirUtilisateur(string idStr);
+        int CreerUnSondage();
+        void AjouterVote(int idSondage, int idResto, int idUtilisateur);
+        List<Resultats> ObtenirLesResultats(int idSondage);
+        bool ADejaVote(int idSondage, string idStr);
     }
 }
