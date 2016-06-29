22e4c3c99b736ae9a82668ede093f07a70f25983
Laurent-Chiaramonte <chiaramonte.laurent@gmail.com>
2016-06-29 15:32:03 +0200

    helpers

---
+++ ChoixResto/ChoixResto/Controllers/AccueilController.cs
@@ -0,0 +1,34 @@
+﻿using ChoixResto.Models;
+using ChoixResto.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChoixResto.Controllers
+{
+    public class AccueilController : Controller
+    {
+        // GET: Accueil
+        public ActionResult Index()
+        {
+            AccueilViewModel vm = new AccueilViewModel
+            {
+                Message = "Bonjour depuis le <span style=\"color:red\">contrôleur</span>",
+                Date = DateTime.Now,
+                Resto = new Resto { Nom = "la bonne fourchette", Telephone = "1234" }
+            };
+            List<Models.Resto> listeDesRestos = new List<Resto>
+            {
+                new Resto { Id = 1, Nom = "Resto pinambour", Telephone = "1234" },
+                new Resto { Id = 2, Nom = "Resto tologie", Telephone = "1234" },
+                new Resto { Id = 5, Nom = "Resto ride", Telephone = "5678" },
+                new Resto { Id = 9, Nom = "Resto toro", Telephone = "555" },
+            };
+            ViewBag.ListeDesRestos = new SelectList(listeDesRestos, "Id", "Nom", 5);
+
+            return View(vm);
+        }
+    }
+}
